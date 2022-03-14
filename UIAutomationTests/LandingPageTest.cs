00f6a908e2a0075229e23fc26e2038e730784223
CrackInWall <stanchev.zdravko87@gmail.com>
2022-03-14 22:13:01 +0200

    adding changes
    1

---
+++ UIAutomationTests/LandingPageTest.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using UIAutomation;
+
+namespace UIAutomationTests
+{
+    public class Tests : TestBase
+    {
+        //[SetUp]
+        //public void Setup()
+        //{
+        //}
+
+        [Test]
+        public void Test1()
+        {
+            //adding  
+            Assert.Fail();
+        }
+    }
+}
